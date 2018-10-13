ffa00368f2608ec21f8ee1f4390a28ffaf9bdda0
jezkie <jezkie00secondary@gmail.com>
2018-10-13 18:43:21 +0800

    added more operation in exercise service

--- src/app/exercises/exercise.service.ts
+++ src/app/exercises/exercise.service.ts
@@ -1,15 +1,17 @@
 import { EventEmitter } from "@angular/core";
 import { Exercise } from "./exercise.model";
+import { Subject } from "rxjs/Subject";
 
 export class ExerciseService {
     private exercises: Exercise[] = [
         new Exercise('Squat', 'Squat', 5, 5),
         new Exercise('Bench Press', 'Bench press', 5, 5)
-      ];
+    ];
 
-    exerciseSelected = new EventEmitter<Exercise>(); 
+    exerciseSelected = new EventEmitter<Exercise>();
+    exercisesChanged = new Subject<Exercise[]>();
 
-    constructor(){}
+    constructor() { }
 
     getExercises() {
         return this.exercises.slice();
@@ -18,5 +20,20 @@ export class ExerciseService {
     getExerciseById(idx: number) {
         return this.exercises[idx];
     }
-    
+
+    addExercise(exercise: Exercise) {
+        this.exercises.push(exercise);
+        this.exercisesChanged.next(this.exercises.slice())
+    }
+
+    updateExercise(idx: number, exercise: Exercise) {
+        this.exercises[idx] = exercise;
+        this.exercisesChanged.next(this.exercises.slice())
+    }
+
+    deleteExercise(idx: number) {
+        this.exercises.splice(idx, 1);
+        this.exercisesChanged.next(this.exercises.slice())
+    }
+
 }
