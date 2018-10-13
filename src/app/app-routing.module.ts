5046239314e7045bb2d9b6cf5a022b8f390bb557
jezkie <jezkie00secondary@gmail.com>
2018-10-13 18:41:37 +0800

    Updated routes, add exercise under exercises component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { Routes, RouterModule } from "@angular/router";
 import { ExercisesComponent } from "./exercises/exercises.component";
 import { ExerciseAddComponent } from "./exercises/exercise-add/exercise-add.component";
 import { ExerciseDetailComponent } from "./exercises/exercise-detail/exercise-detail.component";
+import { LoginComponent } from "./login/login.component";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/exercises', pathMatch: 'full' },
-    { path: 'exercises', component: ExercisesComponent, children: [
-            { path: ':id', component: ExerciseDetailComponent }
+    { path: 'login', component: LoginComponent },
+    {
+        path: 'exercises', component: ExercisesComponent, children: [
+            { path: 'add', component: ExerciseAddComponent },
+            { path: ':id', component: ExerciseDetailComponent },
+
         ]
-    },
-    { path: 'add', component: ExerciseAddComponent }
+    }
 ];
 
 @NgModule({
